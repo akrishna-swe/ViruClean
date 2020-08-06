c915fcb544a4a2db6fcc179b09cb24254d1c6037
Glitch (likeable-descriptive-salesman) <none>
2020-08-06 22:18:03 +0000

    Created inital glitch file

---
+++ script.js
@@ -0,0 +1,17 @@
+// Add to this list as you consult the p5.js documentation for other functions.
+/* global createCanvas, colorMode, HSB, width, height, random, background, fill, 
+          color, random, rect, ellipse, stroke, image, loadImage, keyCode,
+          collideCircleCircle, text, textSize, mouseX, mouseY, strokeWeight, line, 
+          mouseIsPressed, windowWidth, windowHeight, noStroke, UP_ARROW, triangle 
+          frameRate frameCount round startPlants startHerbivores startCarnivores
+          createGui abs*/
+
+
+function setup() {
+   createCanvas(400,400);
+   background(95);
+  }
+
+function draw() {
+  
+  }
